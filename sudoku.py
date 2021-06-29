995d476f797b0b0747064d30bb6fbbbcf0f70c72
Jacob Gilhaus <gilhaus@wustl.edu>
2021-06-28 21:50:17 -0400

    backtracking implemented

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,7 @@
+# sudoku.py | Jacob Gilhaus
+# Sudoku backtracking solver
+# Inspired by Tech with Tim
+
 import requests
 import argparse
 
@@ -15,7 +19,7 @@ elif args.difficulty == 3:
     response = requests.get('https://sugoku.herokuapp.com/board?difficulty=hard')
     board = response.json()['board']
 
-default_board = [
+playing_board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
@@ -26,6 +30,50 @@ default_board = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+for x in range(0,9):
+    for y in range(0,9):
+        playing_board[x][y] = board[x][y]
+
+def solve(b):
+    nxt = find_empty(b)
+    if not nxt:
+        return True
+    else:
+        r, c = nxt
+
+    for i in range(1,10):
+        if valid(b, i, (r,c)):
+            b[r][c] = i
+
+            if solve(b):
+                return True
+
+            b[r][c] = 0
+
+    return False
+            
+def valid(b, num, square):
+    # check the row
+    for c in range(len(b[0])):
+        if b[square[0]][c] == num and square[1] != c:
+            return False
+
+    # check the column
+    for r in range(len(b)):
+        if b[r][square[1]] == num and square[0] != r:
+            return False
+
+    xsquare = square[1] // 3
+    ysquare = square[0] // 3
+
+    # check the subsquare
+    for r in range(ysquare*3, ysquare*3+3):
+        for c in range(xsquare*3, xsquare*3+3):
+            if b[r][c] == num and (r,c) != square:
+                return False
+
+    return True
 
 def print_board(b):
     for x in range(len(b[0])):
@@ -49,4 +97,7 @@ def find_empty(b):
         
     return None
 
-print_board(board)
+print_board(playing_board)
+solve(playing_board)
+print('\n- - - - - - - - - - -\n')
+print_board(playing_board)
